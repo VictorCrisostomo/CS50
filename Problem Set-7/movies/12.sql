a1b5334d3bc20957a1efeda4551f4208f5e74122
Victor Crisostomo Olivieira <victorcrisostomoliveira@gmail.com>
2021-02-20 20:46:52 -0300

    Add files via upload

---
+++ Problem Set-7/movies/12.sql
@@ -0,0 +1,23 @@
+--write a SQL query to list the titles of all movies in which both Johnny Depp and Helena Bonham Carter starred
+
+  
+SELECT movies.title FROM people
+JOIN stars ON people.id = stars.person_id
+JOIN movies ON stars.movie_id = movies.id
+WHERE people.name = "Johnny Depp" AND movies.title IN
+SELECT movies.title FROM people
+JOIN stars ON people.id = stars.person_id
+JOIN movies ON stars.movie_id = movies.id
+WHERE people.name = "Helena Bonham Carter";
+
+--other solution
+
+SELECT movies.title FROM people
+JOIN stars ON people.id = stars.person_id
+JOIN movies ON stars.movie_id = movies.id
+WHERE people.name = "Johnny Depp"
+INTERSECT
+SELECT movies.title FROM people
+JOIN stars ON people.id = stars.person_id
+JOIN movies ON stars.movie_id = movies.id
+WHERE people.name = "Helena Bonham Carter";

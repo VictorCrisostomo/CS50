90f20ee46a6a73ff8e45444610f45998c80d2ae5
Victor Crisostomo Olivieira <victorcrisostomoliveira@gmail.com>
2021-02-20 20:43:08 -0300

    Add files via upload

---
+++ Problem Set-1/mario.c
@@ -0,0 +1,25 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height, row, column, space;
+    do
+    {
+        height = get_int("height; ");
+    }
+    while (height < 1 || height > 8);
+    
+    for (row = 0; row < height; row++)
+    {
+        for (space = 0; space < height - row - 1; space++)
+        {
+            printf(" ");
+        }
+        for (column = 0; column <= row; column++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
